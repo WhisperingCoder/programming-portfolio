bc38759f6bba351c0e8602dd386b70edc61445a0
Shayne Thompson <74395096+WhisperingCoder@users.noreply.github.com>
2023-03-12 18:38:18 -0500

    Push from visual studio code

---
+++ JavaScript/javascript.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+toggleButton.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
